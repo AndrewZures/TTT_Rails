4f802755fe3f94f188ff27a364c748af6fd0c815
Andrew James Zures <andrew.zures@gmail.com>
2013-07-22 09:53:06 -0500

    working gem

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,9 +1,12 @@
 require "two_dimensional_board"
+require "computer_ai"
 
 class GameController < ApplicationController
 
     def new_game
-        @board = TwoDimensionalBoard.new(3)
+        session[:game] = TwoDimensionalBoard.new(4)
+        @board = session[:game]
+        session[:ai] = ComputerAI.new(:o, 100)
         results = @board.get_print_results
         @board_array = results[0];
         @row_length = results[1];
@@ -12,6 +15,21 @@ class GameController < ApplicationController
     def get_board
         @board.get_print_results
     end
+
+    def update
+        @move = params[:move].to_i
+        @board = session[:game]
+        @ai = session[:ai]
+        @board.record_choice(@move, :x)
+        if (@board.check_board_status == :continue_game)
+            @ai_move = @ai.make_move(@board)
+        end
+
+        results = @board.get_print_results
+        @board_array = results[0];
+        @row_length = results[1];
+    end
+
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 TTT::Application.routes.draw do
 
-    root to: "game#new_game", as: :new_gam
-
+    root to: "game#new_game", as: :new_game
+    post "game" => "game#update", as: :update_game
+    
 end
 
