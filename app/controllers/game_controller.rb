6fd4cb636ba1342e42a5161cc15e582fda203662
Andrew James Zures <andrew.zures@gmail.com>
2013-07-22 19:47:27 -0500

    session updates

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,33 +1,65 @@
-require "two_dimensional_board"
-require "computer_ai"
+require "factory"
 
 class GameController < ApplicationController
-
-    def new_game
-        session[:game] = TwoDimensionalBoard.new(4)
-        @board = session[:game]
-        session[:ai] = ComputerAI.new(:o, 100)
-        results = @board.get_print_results
-        @board_array = results[0];
-        @row_length = results[1];
-    end
 
     def get_board
         @board.get_print_results
     end
 
+    def setup_new_game parameters
+        factory = Factory.new
+        session[:game] = factory.get_board(params[:game_type.to_sym])
+        session[:player1] = factory.get_player(params[:player1].to_sym, :o)
+        session[:player2] = factory.get_player(params[:player2], :x)
+        session[:current_player] = :player1
+    end
+
     def update
-        @move = params[:move].to_i
-        @board = session[:game]
-        @ai = session[:ai]
-        @board.record_choice(@move, :x)
-        if (@board.check_board_status == :continue_game)
-            @ai_move = @ai.make_move(@board)
+        if params[:game_type] != nil
+            session[:game] = nil
+            factory = Factory.new
+            session[:game] = factory.get_board(params[:game_type].to_sym)
+            session[:player1] = factory.get_player(params[:player1].to_sym, :o)
+            session[:player2] = factory.get_player(params[:player2].to_sym, :x)
+            session[:current_player] = :player1
+
+        else
+            
+            @move = params[:move].to_i
+            @board = session[:game]
+            if session[:current_player] == :player1
+                @current_player = session[:player1]
+                if @current_player.get_player_type == :human
+                    @board.record_choice(@move, @current_player.symbol)
+                else
+                    @current_player.make_move(@board)
+                end
+            else
+                @current_player = session[:player2]
+                if @current_player.get_player_type == :human
+                    @board.record_choice(@move, @current_player.symbol)
+                else
+                    @current_player.make_move(@board)
+                end
+            end
+            session[:current_player] = opponent(session[:current_player])
+
+
+
         end
 
+        @board = session[:game]
         results = @board.get_print_results
         @board_array = results[0];
         @row_length = results[1];
+        
+
+        render action: :update
+    end
+
+    def opponent player
+        if player == :player1 then :player2
+        else :player1 end
     end
 
 
